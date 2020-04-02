6eb458836157eac524996e1dc871d0cd7bc6215d
Lucas Bertoldo <lucas.fber@gmail.com>
2020-04-02 19:18:18 -0300

    TodoContext created

---
+++ src/context/types.js
@@ -0,0 +1,8 @@
+export const ADD_TODO = "ADD_TODO"
+export const EDIT_TODO = "EDIT_TODO"
+export const DELETE_TODO = "DELETE_TODO"
+export const VIEW_ALL_TODOS = "VIEW_ALL_TODOS"
+export const TOGGLE_EDIT_MODE = "TOGGLE_EDIT_MODE"
+export const CLEAR_ALL = "CLEAR_ALL"
+export const TOGGLE_MODAL_DELETE = "TOGGLE_MODAL_DELETE"
+export const SET_FILTER = "SET_FILTER"
